bde0d2194d6c1763aae2e6a3cb49a8cf7caeeba5
Grishma Shrestha <grishmashrestha@gmail.com>
2019-04-21 12:16:44 +0545

    Use worker to prcess csv, implementation on model and controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,16 +1,23 @@
 module Api
 	module V1
 		class UsersController < ApplicationController
+
 			def index
 				users = User.order('created_at DESC')
 				render json: {status: 'Success', message: 'Loaded users', data: users}, status: :ok
 			end
 
 			def create
-				file =  params[:file]
-				User.import(file)
-				users = User.order('created_at DESC')
-				render json: {status: 'Success', message: 'Loaded users', data: users}, status: :ok
+				file =  params[:file]		
+				name = file.original_filename
+				directory = "storage/"
+				# create the file path
+				path = File.join(directory, name)
+				# write the file
+				csv_file = File.open(path, "wb")
+				csv_file.write(file.tempfile.read)
+				ReportWorker.perform_async(path)
+				render json: {status: 'Success', message: 'Loaded users'}, status: :ok
 			end
 
 		end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,64 +1,60 @@
-require 'csv'
 class User < ApplicationRecord
 	has_many :sons, class_name: "User", foreign_key: "father_id"
  	has_one :father, class_name: "User"
 
-	def self.import(file)
-        CSV.foreach(file.path, col_sep: "\t", headers: true) do |row|
-        	grandfather = User.find_by_full_name(row[0])
-        	father = User.find_by_full_name(row[1])
-        	son = User.find_by_full_name(row[2])
+	def self.import_each(row)
+		grandfather = User.find_by_full_name(row[0])
+        father = User.find_by_full_name(row[1])
+        son = User.find_by_full_name(row[2])
+		if grandfather.nil? 
+			# create new grandfather
+			grandfather = User.create(full_name: row[0])
+			if father.nil?
+				father = User.create(full_name: row[1])
+				grandfather.sons << father
+				
+				son = User.create(full_name: row[2], address: row[3])
+				father.sons << son
+			else 
+				# if father already has a father_id
+				if father.father_id?
+					father = User.create(full_name: row[1])
+				end
+				grandfather.sons << father
 
-        	if grandfather.nil? 
-        		# create new grandfather
-        		grandfather = User.create(full_name: row[0])
-        		if father.nil?
-        			father = User.create(full_name: row[1])
+				son = User.create(full_name: row[2], address: row[3])
+				father.sons << son
+			end
+		else 
+			if father.nil?
+				father = (User.create(full_name: row[1]))
+				grandfather.sons << father
+				son = User.create(full_name: row[2], address: row[3])
+				father.sons << son
+			 else
+				 if grandfather.is_a_father_of(father)
+					 if son.nil?
+						 son = User.create(full_name: row[2], address: row[3])
+						father.sons << son
+					 else
+						 if father.is_a_father_of(son)
+							 # do nothing
+						 else
+							 son = User.create(full_name: row[2], address: row[3])
+							father.sons << son
+						 end
+					 end
+				 else
+					 father = (User.create(full_name: row[1]))
 					grandfather.sons << father
-					
 					son = User.create(full_name: row[2], address: row[3])
 					father.sons << son
-				else 
-					# if father already has a father_id
-					if father.father_id?
-	        			father = User.create(full_name: row[1])
-					end
-					grandfather.sons << father
+				 end
+			end
+		end
+	end
 
-        			son = User.create(full_name: row[2], address: row[3])
-					father.sons << son
-        		end
-        	else 
-        		if father.nil?
-        			father = (User.create(full_name: row[1]))
-        			grandfather.sons << father
-        			son = User.create(full_name: row[2], address: row[3])
-					father.sons << son
-     			else
-     				if grandfather.is_a_father_of(father)
-     					if son.nil?
-     						son = User.create(full_name: row[2], address: row[3])
-							father.sons << son
-     					else
-     						if father.is_a_father_of(son)
-     							# do nothing
-     						else
-     							son = User.create(full_name: row[2], address: row[3])
-								father.sons << son
-     						end
-     					end
-     				else
-     					father = (User.create(full_name: row[1]))
-	        			grandfather.sons << father
-	        			son = User.create(full_name: row[2], address: row[3])
-						father.sons << son
-     				end
-        		end
-        	end
-        end
-    end
-
-    def is_a_father_of(unverified_son)
+	def is_a_father_of(unverified_son)
     	self.sons.include?(unverified_son)
     end
 end
