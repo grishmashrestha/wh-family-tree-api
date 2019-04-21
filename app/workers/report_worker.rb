847c3c0be0237b2ca0f3c3053c3e54ded162163b
Grishma Shrestha <grishmashrestha@gmail.com>
2019-04-21 12:16:16 +0545

    Add worker to process csv

---
+++ app/workers/report_worker.rb
@@ -0,0 +1,16 @@
+class ReportWorker
+	include Sidekiq::Worker
+	sidekiq_options retry: false
+	require 'csv'
+
+	def perform(path)
+		# open the file
+		csv_file = File.open(path, "r")
+		puts "inside worker"
+		sleep (30.seconds)
+		CSV.foreach(csv_file, col_sep: "\t", headers: true) do |row|  
+			puts "inside foreach csv" 	
+			User.import_each(row)
+		end
+	end
+end
